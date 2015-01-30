73063f5d7ab181e315a7a415c456d68d5d79df06
jrhoads <joseph_rhoads@brown.edu>
2015-01-30 13:18:00 -0500

    Add presentation to DjangoAdmin

--- admin.py
+++ admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
+class SlideInline(admin.TabularInline):
+    model = models.Slide
+
+class PresentationAdmin(admin.ModelAdmin):
+    inlines = [
+        SlideInline,
+    ]
 # Register your models here.
+admin.site.register(models.Presentation, PresentationAdmin)
